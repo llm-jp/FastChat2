73bf149c085121f2a39759157acd86d5853dd7a7
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2025-01-29 14:33:09 +0900

    fix

--- scripts/llm-jp-chatbot-arena/setup_neuron.sh
+++ scripts/llm-jp-chatbot-arena/setup_neuron.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# git clone https://github.com/llm-jp/FastChat2.git
-
 source /opt/aws_neuronx_venv_pytorch_2_5_transformers/bin/activate
 
-git clone https://github.com/vllm-project/vllm.git
-cd vllm
-git checkout v0.6.0
-git apply ~/FastChat2/scripts/llm-jp-chatbot-arena/vllm_v0.6.0_neuron.patch
-pip install .
-pip install ray
-cd ..
+git clone -b v0.6.x-neuron https://github.com/aws-neuron/upstreaming-to-vllm.git
+cd upstreaming-to-vllm
+pip install -r requirements-neuron.txt
+VLLM_TARGET_DEVICE="neuron" && pip install -e .
