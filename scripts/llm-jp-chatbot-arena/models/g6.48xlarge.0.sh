6f6e265aeb295733fdab9ffda010c644f36a8a1c
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2025-06-13 17:40:23 +0900

    update

--- scripts/llm-jp-chatbot-arena/models/g6.48xlarge.0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# ssh 10.0.137.196
-./.local/bin/vllm serve Qwen/Qwen2.5-72B-Instruct \
-  --tensor-parallel-size 8 \
-  --max-num-seqs 8 \
-  --max-model-len 4000 \
-  --port 8000
--- scripts/llm-jp-chatbot-arena/models/inf2-24xlarge.sh
+++ scripts/llm-jp-chatbot-arena/models/inf2-24xlarge.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-# ssh 10.0.139.162
+# ssh 10.0.130.47
 source /opt/aws_neuronx_venv_pytorch_2_5_transformers/bin/activate
 
-env NEURON_RT_VISIBLE_CORES="0-7" vllm serve cyberagent/calm3-22b-chat \
-  --tensor-parallel-size 8 \
+env NEURON_RT_VISIBLE_CORES="0-3" vllm serve llm-jp/llm-jp-3.1-13b-instruct4 \
+  --tensor-parallel-size 4 \
   --max-num-seqs 8 \
   --max-model-len 4000 \
   --block-size 4000 \
   --port 8000
 
-env NEURON_RT_VISIBLE_CORES="8-11" vllm serve llm-jp/llm-jp-3-13b-instruct \
-  --tensor-parallel-size 4 \
+env NEURON_RT_VISIBLE_CORES="4-11" vllm serve cyberagent/calm3-22b-chat \
+  --tensor-parallel-size 8 \
   --max-num-seqs 8 \
   --max-model-len 4000 \
   --block-size 4000 \
